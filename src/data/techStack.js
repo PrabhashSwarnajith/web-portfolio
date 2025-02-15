96f48bbb470bee1b160771f976d1b37248b67ef5
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-15 21:37:34 +0530

    add techStack data file with frontend, backend, and tools technologies

---
+++ src/data/techStack.js
@@ -0,0 +1,31 @@
+const techStacks = [
+    // Frontend Technologies
+    { icon: "html.svg", language: "HTML" },
+    { icon: "css.svg", language: "CSS" },
+    { icon: "javascript.svg", language: "JavaScript" },
+    { icon: "reactjs.svg", language: "ReactJS" },
+    { icon: "tailwind.svg", language: "Tailwind CSS" },
+    { icon: "bootstrap.svg", language: "Bootstrap" },
+    
+  
+    // Backend Technologies
+    { icon: "java.svg", language: "Java" },
+    { icon: "spring-boot.svg", language: "Spring-Boot" },
+    { icon: "nodejs.svg", language: "Node JS" },
+    { icon: "expressjs.svg", language: "Express JS" },
+    { icon: "mongodb.svg", language: "MongoDB" },
+    { icon: "postgresql.svg", language: "PostgreSQL" },
+  
+    { icon: "ubuntu.svg", language: "Ubuntu" },
+    { icon: "github.svg", language: "GitHub" },
+    { icon: "docker.svg", language: "Docker" },
+  
+    // Tools and Libraries
+    { icon: "firebase.svg", language: "Firebase" },
+    { icon: "vite.svg", language: "Vite" },
+  
+  
+  ];
+  
+  export default techStacks;
+  
