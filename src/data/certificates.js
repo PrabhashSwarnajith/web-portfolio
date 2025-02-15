b399658ff1aeb0fa5c97a4e398e691dfc97c9695
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-02-15 21:38:09 +0530

    add certificates data file with Java programming course details

---
+++ src/data/certificates.js
@@ -0,0 +1,25 @@
+import java from "../assets/certicates/java.png";
+const certificates = [
+    {
+      id: 1,
+      Title: "Java Programming and Software Engineering Fundamentals",
+      Img: java,
+    },
+    {
+      id: 2,
+      Title: "Java Programming and Software Engineering Fundamentals",
+      Img: java,
+    },
+    {
+      id: 3,
+      Title: "Java Programming and Software Engineering Fundamentals",
+      Img: java,
+    },
+    {
+      id: 4,
+      Title: "Java Programming and Software Engineering Fundamentals",
+      Img: java,
+    }
+  ];
+  
+  export default certificates;
